b75f63a69ec7ce01e595ee694b845e4cf164200c
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-04-07 10:12:50 +0200

    Création du controller UserController

---
+++ src/main/java/fr/fms/contacts/web/UserController.java
@@ -0,0 +1,32 @@
+package fr.fms.contacts.web;
+
+import fr.fms.contacts.dao.ContactRepository;
+import fr.fms.contacts.dao.MessageRepository;
+import fr.fms.contacts.dao.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+  @Autowired
+  UserRepository userRepository;
+
+  @Autowired
+  ContactRepository contactRepository;
+
+  @Autowired
+  MessageRepository articleRepository;
+
+  @Autowired
+  ContactRepository categoryRepository;
+
+  @GetMapping("/index")
+  public String index(Model model) {
+
+    model.addAttribute("listContact", contactRepository.findAll());
+    return "contacts";
+  }
+}
