062b987e367d070c4922a50e264bbb092dd90de4
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-04-08 11:12:58 +0200

    Modification de contactController

--- src/main/java/fr/fms/contacts/web/ContactController.java
+++ src/main/java/fr/fms/contacts/web/ContactController.java
@@ -1,13 +1,15 @@
 package fr.fms.contacts.web;
 
+import fr.fms.contacts.dao.CategorieRepository;
 import fr.fms.contacts.dao.ContactRepository;
 import fr.fms.contacts.dao.UserRepository;
 import fr.fms.contacts.dao.MessageRepository;
+import fr.fms.contacts.entities.Categorie;
 import fr.fms.contacts.entities.Contact;
 import fr.fms.contacts.entities.User;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
-import java.util.Optional;
+import java.util.List;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -32,16 +34,24 @@ public class ContactController {
   MessageRepository articleRepository;
 
   @Autowired
-  ContactRepository categoryRepository;
+  CategorieRepository categorieRepository;
 
   @GetMapping("/index")
   public String index(Model model, HttpSession session) {
 
     Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+    if (auth == null || !auth.isAuthenticated() || auth.getName().equals("anonymousUser")) {
+      model.addAttribute("listContact", null); // ou liste vide
+      return "contacts";
+    }
+
     String username = auth.getName();
     User user = userRepository.findUserByMail(username).orElseThrow(() -> new RuntimeException("Utilisateur non trouvé"));
+
     session.setAttribute("loggedUser", username);
     model.addAttribute("listContact", contactRepository.findContactsByUser(user));
+
     return "contacts";
   }
 
@@ -81,27 +91,23 @@ public class ContactController {
   @GetMapping("/edit")
   public String edit(Long id, Model model){
     Contact contact = contactRepository.findContactById(id);
+    List<Categorie> categories = categorieRepository.findAll();
     model.addAttribute("contact", contact);
+    model.addAttribute("categories",categories);
     return "edit";
   }
 
   @PostMapping("/update")
-  public String update(Model model,
-      @Valid Contact contact,
-      BindingResult bindingResult,
-      @AuthenticationPrincipal UserDetails userDetails) {
+  public String update(Model model, @Valid Contact contact, BindingResult bindingResult, @AuthenticationPrincipal UserDetails userDetails) {
 
     if (bindingResult.hasErrors()) {
+      model.addAttribute("categories", categorieRepository.findAll());
       return "edit";
     }
 
     String mail = userDetails.getUsername();
-
-    User user = userRepository.findUserByMail(mail)
-        .orElseThrow();
-
+    User user = userRepository.findUserByMail(mail).orElseThrow();
     contact.setUser(user);
-
     contactRepository.save(contact);
 
     return "redirect:/index";
