d5ee1c1bbae200280c3b9e6b7fad1de5ad038ed1
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-04-09 09:41:07 +0200

    JavaDoc de la méthode login

--- src/main/java/fr/fms/contacts/web/UserController.java
+++ src/main/java/fr/fms/contacts/web/UserController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
+
+  /*
+  Affiche la page de connexion
+  @return le nom de la vue à afficher ("login")
+   */
   @GetMapping("/login")
   public String login() {
     return "login";
